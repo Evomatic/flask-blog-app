51f6b6c744ec30e4f35666a096421480265fac15
Evan <evantrujillo30@gmail.com>
2024-02-08 13:14:20 +0900

    Create connection

--- frontend-app/src/index.ts
+++ frontend-app/src/index.ts
@@ -1,9 +1,24 @@
 window.onload = () => {
+  // Submit form data
   const formElement = document.getElementById("form") as HTMLFormElement;
   if (formElement) {
-    formElement.addEventListener("submit", (_) => {
+    formElement.addEventListener("submit", async (event: SubmitEvent) => {
+      event.preventDefault();
       const formData = new FormData(formElement);
-      console.log(formData);
+
+      try {
+        const res = await fetch("http://localhost:5001/auth/register", {
+          method: "POST",
+          body: formData,
+        });
+        const resData = await res.json();
+
+        console.log(resData);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          console.log(err.message);
+        }
+      }
     });
   }
 };
